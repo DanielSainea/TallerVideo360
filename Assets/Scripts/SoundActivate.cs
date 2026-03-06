822f3e8059b89331d46805872f1bdeb16070dd4f
DanielSainea <160081641+DanielSainea@users.noreply.github.com>
2026-03-06 16:27:39 -0500

    primeros avances
    primeros avances

---
+++ Assets/Scripts/SoundActivate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activarsonido : MonoBehaviour
+{
+    public GameObject post;
+    public GameObject post1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioManager.Instance.Play3D("sound1", post.transform.position);
+            //AudioManager.Instance.Play2D("");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioManager.Instance.Play3D("sound2", post1.transform.position);
+            //AudioManager.Instance.Play2D("Salir");
+        }
+    }
+}
